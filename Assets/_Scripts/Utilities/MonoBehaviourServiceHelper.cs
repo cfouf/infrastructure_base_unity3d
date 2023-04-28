281128ac3c6a1a7893209b12bc328d2f25f36926
Filipp <fil.agaponov@mail.ru>
2023-04-28 12:41:02 +0500

    Небольшой рефактор
    Стало на один Select меньше, убрана ненужная проверка на typeof(MonoBehaviourService<>)

--- Assets/_Scripts/Utilities/MonoBehaviourServiceHelper.cs
+++ Assets/_Scripts/Utilities/MonoBehaviourServiceHelper.cs
@@ -9,23 +9,19 @@ namespace _Scripts.Utilities
     {
         public static List<T> GetAllInstances<T>()
         {
-            return AppDomain.CurrentDomain
-                .GetAssemblies()
+            return AppDomain.CurrentDomain.GetAssemblies()
                 .SelectMany(assembly => assembly
                     .GetTypes(), (assembly, type) => new {assembly, type})
-                .Where(t =>
-                    typeof(T)
-                        .IsAssignableFrom(t.type) && !t.type.IsInterface &&
-                    t.type != typeof(MonoBehaviourService<>))
+                .Where(t => typeof(T)
+                    .IsAssignableFrom(t.type) && !t.type.IsInterface)
                 .Select(t => typeof(MonoBehaviourService<>)
                     .MakeGenericType(t.type))
                 .Select(genericType => genericType
                     .GetMethod("Get", BindingFlags.Static | BindingFlags.Public))
                 .Where(method => method != null)
-                .Select(method => method
+                .Select(method => (T) method
                     .Invoke(null, new object[] {true}))
                 .Where(instance => instance != null)
-                .Select(instance => (T) instance)
                 .ToList();
         }
     }
